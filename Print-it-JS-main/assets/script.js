ccfd92717b528b13d83a7198ca2f762a6d02393c
marcbenbouik <marc.benbouik1@gmail.com>
2023-05-01 15:04:13 +0200

    automatisation script

--- Print-it-JS-main/assets/script.js
+++ Print-it-JS-main/assets/script.js
@@ -17,36 +17,22 @@ const slides = [
 	}
 ]
 
-// const arrowLeft = document.querySelector(".arrow_left");
-
-// arrowLeft.onclick = function() {window.alert('clic sur la fleche gauche')};
-
-// const arrowRight = document.querySelector(".arrow_right");
-
-// arrowRight.onclick = function() {window.alert('clic sur la fleche droite')};
-
-// const affiche =slides.image;
-
-
 // affichages des photo
-let p =0;
-	const divBanner = document.getElementById("banner");
+var count = 0;
+const divBanner = document.getElementById("banner");
 
-	const imagePhoto = document.createElement("img");
-	imagePhoto.src = slides[p].image;
-	imagePhoto.classList.add("banner-img");
+const imagePhoto = document.createElement("img");
+imagePhoto.src = slides[count].image;
+imagePhoto.classList.add("banner-img");
 
-	const textPhoto = document.createElement("p");
-	textPhoto.innerHTML = slides[p].tagLine;
+const textPhoto = document.createElement("p");
+textPhoto.innerHTML = slides[count].tagLine;
 
-	divBanner.appendChild(imagePhoto);
-	divBanner.appendChild(textPhoto);
+divBanner.appendChild(imagePhoto);
+divBanner.appendChild(textPhoto);
 
-	const arrowRight = document.querySelector(".arrow_right");
-	arrowRight.onclick = function () { 
-		p++;
-	};
-
+const arrowRight = document.querySelector(".arrow_right");
+const arrowLeft = document.querySelector(".arrow_left");
 
 // ajout des div dots
 
@@ -54,73 +40,56 @@ for (let i = 0; i < slides.length; i++) {
 
 	const div = document.createElement("div");
 	div.className = "dot";
-	div.setAttribute('id',i);
+	div.setAttribute('id', i);
 
 	const divDots = document.querySelector(".dots");
 	divDots.appendChild(div);
 }
 
 const div = document.querySelector(".dot");
-const arrowLeft = document.querySelector(".arrow_left");
-
-// arrowLeft.onclick = function () { 
-	
-// 	p--;
-// };
-
-
-// console.log(p);
-
-
-// let a=0;
-// function increment() {
-// 	a++;
-// }
-// const ban = document.getElementById("banner");
-// ban.onclick = increment();
-// console.log(a);
-
-
-var count = 0;
 let dot_selected = document.getElementById(count);
 dot_selected.classList.add("dot_selected");
+const nbrSLide = slides.length;
+const finCarrousel = nbrSLide - 1;
 
 
 // slidedroite
-const handleIncrement = () => {
-	let dot_unselected  = document.getElementById(count);
+const arrowIncrement = () => {
+	let dot_unselected = document.getElementById(count);
 	count++;
-	if(count > 3) {
+	if (count > finCarrousel) {
 		count = 0;
-		dot_unselected = document.getElementById("3");
-	  }
-  console.log(count);
-  imagePhoto.src = slides[count].image;
-  textPhoto.innerHTML = slides[count].tagLine;
-  dot_selected = document.getElementById(count);
-  dot_selected.classList.add("dot_selected");
-  dot_unselected.classList.remove("dot_selected");
+		dot_unselected = document.getElementById(finCarrousel);
+	}
+	// affichage de la photo et du texte
+	imagePhoto.src = slides[count].image;
+	textPhoto.innerHTML = slides[count].tagLine;
+	dot_selected = document.getElementById(count);
+
+	// selection du point et deselection du precedent
+	dot_selected.classList.add("dot_selected");
+	dot_unselected.classList.remove("dot_selected");
 };
 
 // slide gauche
-const handleDecrement = () => {
-	let dot_unselected  = document.getElementById(count);
+const arrowDecrement = () => {
+	let dot_unselected = document.getElementById(count);
 	count--;
-	if(count < 0){
-		count = 3;
-		dot_unselected = document.getElementById("0");
-	  };
-	
-  p = count;
-  imagePhoto.src = slides[p].image;
-  textPhoto.innerHTML = slides[count].tagLine;
-  const dot_selected = document.getElementById(count);
-  dot_selected.classList.add("dot_selected");
-  dot_unselected.classList.remove("dot_selected");
-  console.log(count);
+	if (count < 0) {
+		count = finCarrousel;
+		dot_unselected = document.getElementById(finCarrousel);
+	};
+	// affichage de la photo et du texte
+	imagePhoto.src = slides[count].image;
+	textPhoto.innerHTML = slides[count].tagLine;
+
+	// selection du point et deselection du precedent
+	const dot_selected = document.getElementById(count);
+	dot_selected.classList.add("dot_selected");
+	dot_unselected.classList.remove("dot_selected");
 };
 
 // evenement au clique
-arrowRight.addEventListener("click", handleIncrement);
-arrowLeft.addEventListener("click", handleDecrement);
+arrowRight.addEventListener("click", arrowIncrement);
+arrowLeft.addEventListener("click", arrowDecrement);
 
